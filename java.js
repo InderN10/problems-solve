16b37b331e3cf1e29b9cf7cab04445fac7562492
InderN10 <midtulah2000@gmail.com>
2024-12-02 12:56:34 +0800

    started

---
+++ java.js
@@ -0,0 +1,112 @@
+// // prob1
+// let e=3, r=4, t=5;
+// let s=e+r+t
+// console.log(s)
+// // prob2
+// let a=12, b=23;
+// f=a+b
+// console.log(f)
+// // prob3
+// let v=5
+// h=v*v*v
+// g=(v*v)*6
+// console.log(h)
+// console.log(g)
+// // prob4
+// let p=6, l=4 
+// b=p*l
+// k=(p+l)*2
+// console.log(b)
+// console.log(k)
+// // prob5
+// let x = 1 ;
+// let y = 3*x-5;
+// console.log(y)
+// prob6
+// let x = -2;
+// let y = 4*(x*x)-(3*x)+5;
+// console.log(y)
+// // prob7
+// let n=476, a;
+// a=n%10;
+// console.log(a)
+// // prob8
+// let n=423, a;
+//  n=n%100;
+//  a=n/10;
+
+//   console.log(parseInt(a));
+// // prob9
+// let a = 27, f, b, p;
+// f = a % 10;
+// b = a / 10;
+// p = f * parseInt(b)
+// console.log(f)
+// console.log(parseInt(b))
+// console.log(p)
+// // prob10
+// let a=123, b, c, p, o, h;
+// b=parseInt(a/100);
+// o=a/10;
+// c=parseInt(o)%10;
+// p=a%10;
+// h=b+c+p
+// console.log(h)
+// // prob11
+// let a=200, b, c;
+// b=parseInt(a/60);
+// c=a%60
+// console.log(b)
+// console.log(c)
+// // prob12
+// let a=3612, b, c, n;
+// b=parseInt(a/3600);
+// c=parseInt(a/60);
+// n=a%60;
+// console.log(b)
+// console.log(c)
+// console.log(n)
+// // prob13
+// let a=3, b=4, c, u;
+// c=a*60;
+// u=c+b;
+// console.log(u)
+// // prob14
+// let a=1, b=2, c=3, p;
+// a=a*3600;
+// b=b*60;
+// p=a+b+c
+// console.log(p)
+// // prob15
+// let a=44, b, c;
+// b=parseInt(a/24);
+// c=a%24;
+// console.log(b)
+// console.log(c)
+// // prob16
+// let a=2, b=5, c;
+// a=2*24;
+// c=a+b;
+// console.log(c)
+// // prob17
+// let a=27, c, b;
+// c=parseInt(a/12);
+// b=27-parseInt(c*12)
+// console.log(c)
+// console.log(b)
+// // prob18
+// let a=2, b=7, c;
+// a=a*12;
+// c=a+b;
+// console.log(c)
+// prob19
+let a=16, b=14;
+if (a>b){
+    console.log()
+}
+else{
+    console.log(b)
+}
+// // prob20
+// let a=1, b=3, c=2;
+// if
